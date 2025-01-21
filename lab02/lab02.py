4940487496142cf1338092588dd5e0103d8eb6db
Maxdev18 <maxpersonal1721@gmail.com>
2025-01-21 11:31:51 -0500

    Implemented best_hand function

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -21,7 +21,8 @@ import itertools
 def best_hand(hand):
     "From a 7-card hand, return the best 5 card hand."
     # Your code here
-    pass
+    combinations = list(itertools.combinations(hand, 5))
+    return max(combinations, key=hand_rank)
     
 # ------------------
 # Provided Functions
@@ -87,12 +88,9 @@ def two_pair(ranks):
         return None 
     
 def best_hand_try():
-    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
-            == ['6C', '7C', '8C', '9C', 'TC'])
-    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
-            == ['8C', '8S', 'TC', 'TD', 'TH'])
-    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
-            == ['7C', '7D', '7H', '7S', 'JD'])
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split())) == ['6C', '7C', '8C', '9C', 'TC'])
+    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split())) == ['8C', '8S', 'TC', 'TD', 'TH'])
+    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split())) == ['7C', '7D', '7H', '7S', 'JD'])
     return 'test_best_hand passes'
 
 if __name__ == '__main__':
